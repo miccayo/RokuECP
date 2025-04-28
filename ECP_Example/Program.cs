616f9efd69b3059652c42035f55ba4bed8141913
miccayo <6856974+miccayo@users.noreply.github.com>
2025-04-27 20:30:49 -0500

    Added example project, restructured solution

---
+++ ECP_Example/Program.cs
@@ -0,0 +1,16 @@
+﻿using RokuECP;
+using System.Net;
+using System.Reflection;
+
+namespace ECP_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example usage
+            RokuPlayer myPlayer = new(IPAddress.Parse("192.168.1.69"));
+            Console.WriteLine("This device's serial number is {0}.", myPlayer.SerialNumber);
+        }
+    }
+}
---
+++ RokuECPTests/RokuPlayerTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Net;
+
+namespace RokuECP.Tests
+{
+    [TestClass()]
+    public class RokuPlayerTests
+    {
+        // To run tests successfully, we need an actual Roku device IP and an invalid (non-Roku) device
+        private readonly IPAddress ValidIpAddress = IPAddress.Parse("192.168.1.69");
+        private readonly ushort ValidPort = 8060;
+        private readonly IPAddress InvalidIpAddress = IPAddress.Parse("10.0.0.1");
+        private readonly ushort InvalidPort = 65535;
+
+        [TestMethod]
+        public void InitializePlayerWithValidIp()
+        {
+            RokuPlayer player = new(ValidIpAddress);
+            Assert.IsTrue(player.UDN != string.Empty);
+        }
+
+        [TestMethod]
+        public void InitializePlayerWithValidIpAndValidPort()
+        {
+            RokuPlayer player = new(ValidIpAddress);
+            Assert.IsTrue(player.UDN != string.Empty);
+        }
+
+        [TestMethod]
+        public void InitializePlayerWithInvalidIp()
+        {
+            Assert.ThrowsException<TaskCanceledException>(() =>
+            {
+                RokuPlayer player = new(InvalidIpAddress);
+            });
+        }
+
+        [TestMethod]
+        public void InitializePlayerWithValidIpAndInvalidPort()
+        {
+            Assert.ThrowsException<HttpRequestException>(() =>
+            {
+                RokuPlayer player = new(ValidIpAddress, InvalidPort);
+            });
+        }
+
+        [TestMethod]
+        public void InitializePlayerWithInvalidIpAndValidPort()
+        {
+            Assert.ThrowsException<TaskCanceledException>(() => { _ = new RokuPlayer(InvalidIpAddress, ValidPort); });
+        }
+
+        [TestMethod]
+        public void InitializePlayerWithInvalidIpAndInvalidPort()
+        {
+            Assert.ThrowsException<TaskCanceledException>(() => { _ = new RokuPlayer(InvalidIpAddress, InvalidPort); });
+        }
+    }
+}
